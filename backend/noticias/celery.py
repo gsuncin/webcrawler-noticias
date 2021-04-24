57d56c20d36612622b40bb942dd93719193671e3
Gabriel Suncin <suncin27@gmail.com>
2021-04-24 17:41:47 -0300

    updating dashboard material

--- backend/noticias/celery.py
+++ backend/noticias/celery.py
@@ -1,11 +1,13 @@
 import os
 from celery import Celery
 from django.conf import settings
+from .settings import BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-app = Celery('noticias.celery')
+app = Celery('core')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
 
 
 @app.task(bind=True)
